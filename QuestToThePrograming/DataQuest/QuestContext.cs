df76b610c57b970ba5365d986c4e2e39a6aebeb3
vase_ok 1 <vvaseokk@gmail.com>
2021-02-25 13:38:08 +0200

    Add project files.

---
+++ QuestToThePrograming/DataQuest/QuestContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using QuestToThePrograming.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QuestToThePrograming.DataQuest
+{
+    public class QuestContext : DbContext 
+    {
+        public QuestContext(DbContextOptions<QuestContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+        public DbSet<Quest> Quests { get; set; }
+    }
+}
---
+++ QuestToThePrograming/Models/Quest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QuestToThePrograming.Models
+{
+    public class Quest
+    {
+        public int Id { get; set; }
+        public string Question { get; set; }
+        public string Answer { get; set; }
+        public string Wrong1 { get; set; }
+        public string Wrong2 { get; set; }
+        public string Wrong3 { get; set; }
+        public string Language { get; set; }
+        public string TopQuest { get; set; }
+        public int NumberQuest { get; set; }
+        public string Description { get; set; }
+    }
+}
